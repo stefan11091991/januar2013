66e8a89949d9d38d0f45ca77bad6733ebfc450ba
raso354 <raso354@live.com>
2015-01-24 22:37:24 +0100

    menjao prikaz statistike.

--- prikazStatistike.php
+++ prikazStatistike.php
@@ -1,9 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: raso
- * Date: 1/24/2015
- * Time: 6:07 PM
- */
-echo "Prikaz Statistike";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+
+
+</head>
+<body>
+
+<div ng-app="" ng-controller="statistikaController">
+
+    Username: <input type="text" ng-model="userName"><br>
+    Password: <input type="password" ng-model="password"><br>
+    <br>
+    <button ng-click="login()">login</button>
+
+
+</div>
+<script>
+    function statistikaController($scope, $http)
+    {
+        $http.get("server.php/?type=statistika").success(function(response)
+        {
+            alert(JSON.stringify(response));
+        });
+
+
+    }
+</script>
+
+</body>
+</html>
